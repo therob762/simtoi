f6c3d001100bb766d4f1255cb389eea424783ce4
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-08-20 17:20:18 -0400

    Adding new GUI elements.

---
+++ src/QT/CWorker.cpp
@@ -0,0 +1,18 @@
+/*
+ * CWorker.cpp
+ *
+ *  Created on: Aug 20, 2014
+ *      Author: bkloppenborg
+ */
+
+#include "CWorker.h"
+
+CWorker::CWorker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+CWorker::~CWorker() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/QT/CWorker.h
@@ -0,0 +1,19 @@
+/*
+ * CWorker.h
+ *
+ *  Created on: Aug 20, 2014
+ *      Author: bkloppenborg
+ *
+ *  A worker for SIMTOI which is capable of OpenGL and OpenCL tasks.
+ */
+
+#ifndef CWORKER_H_
+#define CWORKER_H_
+
+class CWorker {
+public:
+	CWorker();
+	virtual ~CWorker();
+};
+
+#endif /* CWORKER_H_ */
