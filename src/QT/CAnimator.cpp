36d4e2be18cbb3f684744911e82acf184ba6222e
Brian Kloppenborg <bkloppenborg@gmail.com>
2014-08-29 13:45:19 -0400

    Animiator now renders at specific times.

--- src/QT/CAnimator.cpp
+++ src/QT/CAnimator.cpp
@@ -33,7 +33,7 @@ void CAnimator::run()
 
 	while(mRun)
 	{
-		WorkPtr op = make_shared<WorkItem>(RENDER_TO_SCREEN);
+		WorkPtr op = make_shared<RenderAtTime>(mTime);
 		mQueue->push(op);
 
 		this->msleep(50);
